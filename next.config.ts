b932b090aea13c51cd72a0ee741d2cb4a8fdcca3
Kristijan Trtinjak <trtinjak.k@gmail.com>
2025-07-14 12:16:55 +0000

    nije riješilo problem, nema ikone

--- next.config.ts
+++ next.config.ts
@@ -11,16 +11,6 @@ const nextConfig: NextConfig = {
   experimental: {
   },
   allowedDevOrigins: ["*.cloudworkstations.dev"],
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
 };
 
 export default nextConfig;
